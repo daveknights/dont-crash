4fb804f69869c925c1dfb592914e50a5970b203d
David Knights <david.knights@vcars.co.uk>
2022-05-29 18:19:27 +0100

    Initialise cars in setup, fix lane 2 collision logic

--- sketch.js
+++ sketch.js
@@ -1,23 +1,9 @@
 const laneXs = [-35, 0 ,35];
-let carX, car1X, car2X, newXs, fillColour, timer;
+let myCar, car1, car2, myCarX, car1X, car2X, newXs, fillColour, timer;
 
 function preload() {
   timer = loadFont('roboto.ttf');
 }
-
-function setup() {
-    const canvas = createCanvas(400, 450, WEBGL);
-    canvas.position(innerWidth/2 - width/2);
-    newXs = [];
-    car1X = laneXs[0];
-    car2X = laneXs[1];
-    carX = 0;
-    fillColour = 'white';
-    perspective(1, 1, 0.1, 500);
-    textFont(timer);
-    textSize(24);
-    textAlign(LEFT, CENTER);
-  }
 
 function car() {
     return {
@@ -68,7 +54,7 @@ function collisionTest(carX, emptyLane) {
             carX > -33 && (hasCollided = true);            
             break;
         case 2:
-            (carX < -2 || carX > 37) && (hasCollided = true);           
+            (carX < -5 || carX > 5) && (hasCollided = true);
             break;
         case 3:
             carX < 33 && (hasCollided = true);
@@ -79,56 +65,70 @@ function collisionTest(carX, emptyLane) {
 
     return hasCollided;
 }
+
+function setup() {
+    const canvas = createCanvas(400, 450, WEBGL);
+    canvas.position(innerWidth/2 - width/2);
+    myCar = car();
+    car1 = car();
+    car2 = car();
+    newXs = [];
+    car1X = laneXs[0];
+    car2X = laneXs[1];
+    myCarX = 0;
+    fillColour = 'white';
+    perspective(1, 1, 0.1, 500);
+    textFont(timer);
+    textSize(24);
+    textAlign(LEFT, CENTER);
+  }
   
 function draw() {
-    const myCar = car();
-    const car1 = car();
-    const car2 = car();
-
-    background(200);    
+    background(200);
     rotateX(-0.2);  
-
+    // Road lanes
     stroke(0)
     line(-215, 20, 215, 20);
     line(-60, 20, -195, 245);
     line(-18, 20, -71, 245);
     line(18, 20, 71, 245);
     line(60, 20, 195, 245);
-
+    // Timer
     fill(0, 250, 0);
     text(int(millis()/1000), -180, -180);
-
+    // Traffic
     fill('white');    
     car1.coords = [car1X, 0, frameCount * 3.75];
     car1.show();
     car2.coords = [car2X, 0, frameCount * 3.75];
     car2.show();
-
+    // My car
     fill(fillColour);
-    myCar.coords = [carX, 0, 260];
+    myCar.coords = [myCarX, 0, 260];
     myCar.show();    
-
+    // Controls
     if (keyIsDown(LEFT_ARROW)) {
-        if (carX < -35) {
-            cax = -35;
+        if (myCarX <= -35) {
+            myCarX = -35;
         } else {
-            carX -= 1;
+            myCarX -= 1;
         }        
     }
     if (keyIsDown(RIGHT_ARROW)) {
-        if (carX > 35) {
-            cax = 35;
+        if (myCarX >= 35) {
+            myCarX = 35;
         } else {
-            carX += 1;
+            myCarX += 1;
         } 
     }
+    // Set new traffic position
     if (frameCount === 120)  {
         newXs = shuffle(laneXs)
         frameCount = 0;
         car1X = newXs[0];
         car2X = newXs[1];
     }
-
+    // Check for collisions
     if ((car1.coords.z > 230 && car1.coords.z < 260) && collisionTest(myCar.coords.x, laneXs.indexOf(newXs[2]) + 1)){        
         fillColour = 'red';
     } else {
